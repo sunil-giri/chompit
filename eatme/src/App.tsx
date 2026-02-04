12a031b4425e0248a4c7279dbc2c165fe9c23b20
Sunil Giri <35769623+sunil-giri@users.noreply.github.com>
2026-02-04 23:58:26 +0530

    initial commit

---
+++ eatme/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='min-h-screen bg-slate-100'>
+        <p className='text-xl text-slate-500 font-bold leading-normal tracking-wide '>Test</p>
+      </div>
+    </>
+  )
+}
+
+export default App
